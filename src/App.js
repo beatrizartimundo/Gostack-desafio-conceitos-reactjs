c2c9be705c32f04c62c0818682dba769e5bc7b12
Beatriz Artimundo <beatriz.artimundo@gmail.com>
2020-10-27 07:44:13 -0300

    adicionar itens e botao delete por item

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from "react";
-
+import React,{useState,useEffect} from "react";
 import "./styles.css";
+import api from './services/api';
 
 function App() {
+
+  const [repositories,setRepository] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response =>{
+      setRepository(response.data)
+    })
+  },[])
+
   async function handleAddRepository() {
     // TODO
+    const response = await api.post('repositories',{
+      title:`Novo projeto ${Date.now()}`
+    })
+    const repository = response.data;
+    setRepository([...repositories,repository])
   }
 
-  async function handleRemoveRepository(id) {
+  async function handleRemoveRepository() {
     // TODO
+    //  const request = await api.delete('repository',{})
+
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        
+          {repositories.map(repository => 
+          <li key={repository.id}>
+            {repository.title}<button onClick={() => handleRemoveRepository(1)}>
             Remover
-          </button>
-        </li>
+          </button></li>)}
+          
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
